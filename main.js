3322b7a27cb9a1aeb4d32d089fbeeb24206bc812
Ventura087 <128320009+Ventura087@users.noreply.github.com>
2023-03-19 21:18:41 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+var moveButton = document.querySelector(".errado");
+var botaoCerto = document.querySelector(".certo");
+var botaoVoltar = document.querySelector(".voltar");
+
+var telaUm = document.querySelector(".container-principal");
+var telaDois = document.querySelector(".container-segundario");
+
+var clicou = 0
+
+moveButton.addEventListener("click", function () {
+var vertical = Math.floor(Math.random() * 400);
+var horizontal = Math.floor(Math.random() * 400);
+moveButton.style.position = "absolute";
+moveButton.style.right = vertical + "px";
+moveButton.style.top = horizontal + "px";
+
+
+});
+
+botaoCerto.addEventListener("click", function() {
+    telaUm.style.display = "none";
+    telaDois.style.display = "block"
+})
+
+botaoVoltar.addEventListener("click", function() {
+    telaUm.style.display = "flex";
+    telaDois.style.display = "none";
+})
